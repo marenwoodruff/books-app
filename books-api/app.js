beea2cf71e73f9394f28d7d52d693718bdc3c308
MarenWoodruff <wren403@gmail.com>
2018-11-05 15:10:24 -0500

    post method working

--- books-api/app.js
+++ books-api/app.js
@@ -21,9 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next()
 })
 
--- books-app/src/components/Books.js
+++ books-app/src/components/Books.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import Book from './Book';
 
+let booksApi = 'http://localhost:3001/api/books';
+
 class Books extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      books: []
+      books: [],
+      title: '',
+      author: '',
+      publication_year: '',
+      read: false
     }
   }
 
   componentDidMount() {
-    let booksApi = 'http://localhost:3001/api/books';
     fetch(booksApi)
       .then(res => res.json())
       .then(books => {
-        console.log('books, pre state', books);
         this.setState({
           books
         }, () => {
@@ -23,6 +27,71 @@ class Books extends Component {
         })
       })
       .catch(err => console.log(err));
+  }
+
+  onInputChange(e) {
+    // console.log('target name ', e.target.name)
+    // console.log('target value ', e.target.value)
+    switch(e.target.name) {
+      case 'title':
+        this.setState({
+          title: e.target.value
+        })
+        break;
+      case 'author':
+        this.setState({
+          author: e.target.value
+        })
+        break;
+      case 'publication_year':
+        this.setState({
+          publication_year: e.target.value
+        })
+        break;
+      case 'read':
+        this.setState({
+          read: e.target.value
+        })
+        break;
+      default:
+        console.log('please enter valid information');
+    }
+  }
+
+  onHandleSubmit(e) {
+    e.preventDefault();
+    let formBody = {
+      title: this.state.title,
+      author: this.state.author,
+      publication_year: this.state.publication_year,
+      read: this.state.read
+    }
+
+    console.log('hit the submit');
+
+    let options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formBody)
+    }
+
+    fetch(booksApi, options)
+      .then(res => res.json())
+      .then(book => {
+        this.setState((prevState) => {
+          return {
+            books: [...prevState.books, book],
+            title: '',
+            author: '',
+            publication_year: '',
+            read: false
+          }
+        })
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -33,6 +102,29 @@ class Books extends Component {
     return (
       <div>
         <h1>My Favorite Books!</h1>
+
+        <h3>Add a new book!</h3>
+
+        <form onSubmit={e => this.onHandleSubmit(e)}>
+          <div>
+            <label htmlFor="title">title</label>
+            <input type="text" name="title" id="title" value={this.state.title} onChange={e => this.onInputChange(e)} />
+          </div>
+          <div>
+            <label htmlFor="author">author</label>
+            <input type="text" name="author" id="author" value={this.state.author} onChange={e => this.onInputChange(e)} />
+          </div>
+          <div>
+            <label htmlFor="publication_year">publication year</label>
+            <input type="text" name="publication_year" id="publication_year" value={this.state.publication_year} onChange={e => this.onInputChange(e)} />
+          </div>
+          <div>
+            <label htmlFor="read">have you read it?</label>
+            <input type="text" name="read" id="read" value={this.state.read} onChange={e => this.onInputChange(e)} />
+          </div>
+          <input type="submit" value="submit" />
+        </form>
+
         <ul>
           {books}
         </ul>
