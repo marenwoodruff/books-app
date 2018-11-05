1c923d7a1069deb045b83479328bb66403d014f1
MarenWoodruff <wren403@gmail.com>
2018-11-05 12:14:16 -0500

    updated port for express app. created a Books component. rendering Books component on App.js

--- books-app/src/components/App.js
+++ books-app/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Books from './Books';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My Favorite Books!</h1>
+        <Books />
       </div>
     );
   }
---
+++ books-app/src/components/Books.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Books extends Component {  
+  render() {
+    return (
+      <div>
+        <h1>My Favorite Books!</h1>
+        <ul>
+          <li>Book title</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Books;
